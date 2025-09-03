6fd3257a64210de2eb6c461b96aef8c48c0e1931
Luis Gomez <luisgomez24g@gmail.com>
2025-09-02 23:32:14 -0500

    corrected classes

--- src/app/shared/constants/app.constants.ts
+++ src/app/shared/constants/app.constants.ts
@@ -6,7 +6,7 @@ export const CLASSES_BUTTON =
 
 export const STYLE_BLACK = 'bg-gray-200 text-black hover:shadow-gray-600';
 export const STYLE_RED = 'bg-gray-200 text-red-600 hover:shadow-red-600';
-export const STYLE_PRIMARY = 'bg-primary text-white hover:shadow-primary/70';
-export const STYLE_BLUE = 'bg-blue-600 text-white hover:shadow-blue-600/70';
-export const STYLE_YELLOW = 'bg-yellow-400 text-white hover:shadow-yellow-400/70';
-export const STYLE_TRANSPARENT = 'bg-transparent text-black hover:bg-gray-100';
+export const STYLE_PRIMARY = 'bg-gray-200 text-green-800 hover:shadow-green-600';
+export const STYLE_BLUE = 'bg-gray-200 text-blue-600 hover:shadow-blue-600';
+export const STYLE_YELLOW = 'bg-gray-200 text-yellow-500 hover:shadow-yellow-600';
+export const STYLE_TRANSPARENT = 'bg-transparent text-black shadow-none hover:bg-gray-100';
