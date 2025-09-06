f48ed91c255c3a86746a775dda4c699cc8603e54
Luis Gomez <luisgomez24g@gmail.com>
2025-09-05 22:56:32 -0500

    refactor view mobile

--- src/app/shared/constants/app.constants.ts
+++ src/app/shared/constants/app.constants.ts
@@ -3,7 +3,7 @@ export const PAGE_SIZE_RESERVATIONS = 9;
 export const PAGE_SIZE_FIELDS = 8;
 
 export const CLASSES_BUTTON =
-  'shadow transition-shadow duration-300 ease-in-out rounded-md disabled:cursor-not-allowed disabled:opacity-50 px-4 py-2 z-10 relative';
+  'shadow transition-shadow duration-300 ease-in-out rounded-md disabled:cursor-not-allowed disabled:opacity-50 px-4 py-2 z-10 relative leading-none';
 
 export const STYLE_BLACK = 'bg-black/50 text-gray-200 hover:shadow-gray-600';
 export const STYLE_RED = 'bg-red-600/50 text-white hover:shadow-red-600';
