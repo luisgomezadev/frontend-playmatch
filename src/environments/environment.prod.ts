be74e89c7f45cd968867c019f3b6327223494314
Luis Gomez <luisgomez24g@gmail.com>
2025-09-14 19:06:43 -0500

    fix link deploy production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://playmatch.onrender.com/api/v1',
   authUrl: 'https://playmatch.onrender.com/auth',
-  deploy: 'https://playmatch.onrender.com/'
+  deploy: 'https://playmatch-lgsoftworks.vercel.app/'
 };
